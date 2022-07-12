f26e62b21a3c870478b065a8e029d65d1044734c
Sundram <sundramtpvc@gmail.com>
2022-07-12 18:13:44 +0530

    #Logout Confirmation

--- ServiceDesk_WebApp/wwwroot/js/DashBoard.js
+++ ServiceDesk_WebApp/wwwroot/js/DashBoard.js
@@ -14,5 +14,5 @@ window.addEventListener('DOMContentLoaded', event => {
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
-
+  
 });
